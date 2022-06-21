4c92f41ce69f2c2075b9f8515d29d694caeffe68
pamphilemkp <pamphilemkp@gmail.com>
2022-06-21 12:49:59 +0300

    linters errors amended

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const NavigationMenu = document.querySelector('.mobile-img')
-const hamburgeurMenu = document.querySelector('#header-menu')
-const headerMenu = document.querySelector('.remove')
+const NavigationMenu = document.querySelector('.mobile-img');
+const hamburgeurMenu = document.querySelector('#header-menu');
+const headerMenu = document.querySelector('.remove');
 
-const removeMenuOption = document.querySelectorAll(".link").forEach(a => a.addEventListener("click", () => {
-    hamburgeurMenu.classList.remove("hamburgeur")
-}))
+document.querySelectorAll('.link').forEach((a) => a.addEventListener('click', () => {
+  hamburgeurMenu.classList.remove('hamburgeur');
+}));
 
-NavigationMenu.addEventListener("click",menubar)
-headerMenu.addEventListener("click",menubar)
-removeMenuOption.addEventListener("click",removeMenubar)
 function menubar() {
-    hamburgeurMenu.classList.toggle("hamburgeur")
+  hamburgeurMenu.classList.toggle('hamburgeur');
 }
+
+NavigationMenu.addEventListener('click', menubar);
+headerMenu.addEventListener('click', menubar);
